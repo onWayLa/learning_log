6f5a3e841289ed6babd8cc85b231404bdf020c5c
lah <liuanhaiMJ@163.com>
2019-08-21 00:54:41 +0800

    add new theme and  content

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -15,4 +15,8 @@ urlpatterns = [
     # url(r'^topics/(?P<topic_id>\d+)/$', views.topic,name='topic'),
     re_path(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic'),
     # url(r'topics/(?P<topic_id>\d+)/$', views.topic, name='topic')
+    # 用于添加新的主题网页
+    re_path(r'^new_topic/$', views.new_topic, name='new_topic'),
+    # 用于添加新条目的页面
+    re_path(r'^new_entry/(?P<topic_id>\d+)/$', views.new_entry, name='new_entry'),
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from .models import Topic
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from .forms import TopicForm, EntryForm
 # Create your views here.
 
 
@@ -21,3 +24,36 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
+
+
+def new_topic(request):
+    """添加新主题"""
+    if request.method != 'POST':
+        # 未提交数据：创建一个新表单
+        form = TopicForm()
+    else:
+        # POST提交的数据，对数据进行处理
+        form = TopicForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('learning_logs:topics'))
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
+
+
+def new_entry(request, topic_id):
+    """在特定的主题添加新条目"""
+    topic = Topic.objects.get(id=topic_id)
+    if request.method != 'POST':
+        # 未提交数据，创建一个空表单
+        form = EntryForm()
+    else:
+        # POST 提交数据，对数据进行处理
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return HttpResponseRedirect(reverse('learning_logs:topic', args=[topic_id]))
+    context = {'topic': topic, 'form': form}
+    return render(request, 'learning_logs/new_entry.html', context)
